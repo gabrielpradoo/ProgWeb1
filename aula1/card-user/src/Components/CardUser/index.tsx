20cf29a0eb780f391b05fac28f6c6460aced0442
Gabriel <>
2023-04-04 21:19:01 -0400

    feat: :construction: Stats Area Created and Card Style finished

--- aula1/card-user/src/Components/CardUser/index.tsx
+++ aula1/card-user/src/Components/CardUser/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { BsFacebook, BsInstagram, BsYoutube, BsTwitter } from 'react-icons/bs'
+import { RiHeartFill, RiSendPlaneFill } from 'react-icons/ri'
+import { FaComment } from 'react-icons/fa'
 import './styles.css'
 
 interface UserProps {
@@ -29,6 +31,11 @@ export default function CardUser({ user }: Props) {
             <div className="header-blue">
 
             </div>
+            <div className="image">
+                <div className="profile-img">
+                    <span className="initials">{user.iniciais}</span>
+                </div>
+            </div>
             <div className="content">
                 <div className="user-data">
                     <h2>{user.nome}</h2>
@@ -37,10 +44,10 @@ export default function CardUser({ user }: Props) {
                     <p>Aniversário: {user.nascimento}</p>
                 </div>
                 <div className="social">
-                    <button><BsFacebook size="30" color="#3939f5" /></button>
-                    <button><BsTwitter size="30" color="#3939f5" /></button>
-                    <button><BsInstagram size="30" color="#3939f5" /></button>
-                    <button><BsYoutube size="30" color="#3939f5" /></button>
+                    <button><BsFacebook size="30" color="#1877F2" /></button>
+                    <button><BsTwitter size="30" color="#1DA1F2" /></button>
+                    <button><BsInstagram size="30" color="#E4405F" /></button>
+                    <button><BsYoutube size="30" color="#CD201F" /></button>
                 </div>
                 <div className="body-data">
                     <div>
@@ -56,8 +63,22 @@ export default function CardUser({ user }: Props) {
                         <p>{user.imc}</p>
                     </div>
                 </div>
-            </div>
 
+            </div>
+            <div className="user-stats">
+                <span>
+                    <RiHeartFill size="25" />
+                    <p>{user.likes}</p>
+                </span>
+                <span>
+                    <FaComment size="25" />
+                    <p>{user.peso}</p>
+                </span>
+                <span>
+                    <RiSendPlaneFill size="25" />
+                    <p>{user.imc}</p>
+                </span>
+            </div>
         </div>
     )
 }
